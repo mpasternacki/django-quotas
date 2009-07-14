97ab93da8ca1033763ec16d0bd4d2510c8549589
Maciej Pasternacki <maciej@pasternacki.net>
2009-07-14 12:04:19 +0200

    Monkeypatch User.quotas property instead of user.get_quota() method.

--- quotas/models.py
+++ quotas/models.py
@@ -2,20 +2,35 @@ from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
 
-def __user_get_quota(user, base):
+from quotas import INFINITY
+
+QUOTAS = getattr(settings, 'QUOTAS', {})
+
+def _user_get_quota(user, base):
     """Return user's quota for `base'.
 
     Returns True if unlimited, integer if limited, None if no quota was set."""
     def __has_quota_of(limit):
         return user.has_perm( 'quotas.quota_%s_%s'%(base, limit) )
-    if __has_quota_of('unlimited'): return True
+    if __has_quota_of('unlimited'): return INFINITY
     try:
-        for v in sorted( settings.QUOTAS[base], reverse=True ):
+        for v in sorted( QUOTAS[base], reverse=True ):
             if __has_quota_of(v):
                 return v
     except KeyError: pass               # no base quota
-    except AttributeError: pass         # no settings.QUOTAS
     return None
+# User.add_to_class('get_quota', _user_get_quota)
 
-User.add_to_class('get_quota', __user_get_quota)
+class UserQuotas(object):
+    def __init__(self, user):
+        self.__user = user
+    def __getattr__(self, quota_name):
+        if quota_name in QUOTAS:
+            return _user_get_quota(self.__user, quota_name)
+        raise AttributeError
+
+def _user_get_quotas(user):
+    return UserQuotas(user)
+
+User.add_to_class('quotas', property(_user_get_quotas))
 
