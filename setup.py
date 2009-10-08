3221cf24bb4a789da8d4bfae12b4bfc83f5ac95e
Maciej Pasternacki <maciej@pasternacki.net>
2009-10-08 11:41:43 +0200

    Correct author email.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name="django-quotas",
            version="0.1",
            description="Quota permissions for Django",
            author="Maciej Pasternacki",
-           author_email="john@handimobility.ca",
+           author_email="maciej@pasternacki.net",
            packages=find_packages(),
            include_package_data=True,
 )
